cb8f9a6d1cbf67ed610bf5612ea802b9b632c401
siddheshpatil777 <siddheshpatil777@gmail.com>
2023-08-26 17:49:56 +0530

    tetsigng sid auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import React, { Component } from "react";
+// import { Routes, Route, Link } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
+
 import './App.css';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+
 import AboutUs from './components/AboutUs';
 import CustomerLogin from './components/CustomerLogin';
 import CustomerRegistration from './components/CustomerRegistration';
@@ -8,45 +13,187 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 
-function App() {
-  return (
-    <div className="App">
-      
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-       <h1>Fargo Loans</h1>
-      
-      <section>
-        <div style={{ backgroundImage: "url(/images/pms1.webp)",
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize:'cover', minHeight:'100vh',minWidth:'100vw'}}> 
-         <Router>
-            <Navbar></Navbar>
-              <Routes>
-                  <Route path='/' exact Component={Home}></Route>
-                  <Route path='register' Component={CustomerRegistration}></Route>
-                  <Route path='login' Component={CustomerLogin}></Route>
-                  {/* <Route path='dashboard' Component={Dashboard}></Route> */}
-                  <Route path='aboutus' Component={AboutUs}></Route>
-                  <Route
-                  path="dashboard"
-                  element={
-                    <PrivateRoute>
-                      <Dashboard/>
-                    </PrivateRoute>
-                  }
-                  />
-                  {/* <PrivateRoute path="dashboard" element = {<Dashboard/>}/> */}
-                   
-              </Routes>
-          </Router> 
-        </div>
-      </section>
+import AuthService from "./service/auth.service";
+import EventBus from "./common/EventBus";
 
-       <footer className='footer'>
-        <p>&copy; All Right Reserved to Wells Fargo</p>
-      </footer> 
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+      redirectToLogin:false
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getAuthUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN")
+      });
+    }else{
+      this.setState({
+        redirectToLogin:false
+      });
+      
+    }
+    
+    
+    EventBus.on("logout", () => {
+      this.logOut();
+    });
+  }
+
+  componentWillUnmount() {
+    EventBus.remove("logout");
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    // if(this.state.redirectToLogin){
+    //   // return <Navigate to="/login" />
+    //   return (<Navigate to="/login" replace={true} />)
+    // }
+    return (
+      <div className="App">
+      
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+     <h1>Fargo Loans</h1>
+    
+    <section>
+      <div style={{ backgroundImage: "url(/images/pms1.webp)",
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize:'cover', minHeight:'100vh',minWidth:'100vw'}}> 
+       <Router>
+       {this.state.redirectToLogin && <Navigate to="/login" replace={true} />}
+          <Navbar></Navbar>
+            <Routes>
+                <Route path='/' exact Component={Home}></Route>
+                <Route path='register' Component={CustomerRegistration}></Route>
+                <Route path='login' Component={CustomerLogin}></Route>
+                {/* <Route path='dashboard' Component={Dashboard}></Route> */}
+                <Route path='aboutus' Component={AboutUs}></Route>
+                <Route
+                path="dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard/>
+                  </PrivateRoute>
+                }
+                />
+                {/* <PrivateRoute path="dashboard" element = {<Dashboard/>}/> */}
+                 
+            </Routes>
+        </Router> 
+      </div>
+    </section>
+
+     <footer className='footer'>
+      <p>&copy; All Right Reserved to Wells Fargo</p>
+    </footer> 
+  </div>
+      // <div>
+      //   <nav className="navbar navbar-expand navbar-dark bg-dark">
+      //     <Link to={"/"} className="navbar-brand">
+      //       bezKoder
+      //     </Link>
+      //     <div className="navbar-nav mr-auto">
+      //       <li className="nav-item">
+      //         <Link to={"/home"} className="nav-link">
+      //           Home
+      //         </Link>
+      //       </li>
+
+      //       {showModeratorBoard && (
+      //         <li className="nav-item">
+      //           <Link to={"/mod"} className="nav-link">
+      //             Moderator Board
+      //           </Link>
+      //         </li>
+      //       )}
+
+      //       {showAdminBoard && (
+      //         <li className="nav-item">
+      //           <Link to={"/admin"} className="nav-link">
+      //             Admin Board
+      //           </Link>
+      //         </li>
+      //       )}
+
+      //       {currentUser && (
+      //         <li className="nav-item">
+      //           <Link to={"/user"} className="nav-link">
+      //             User
+      //           </Link>
+      //         </li>
+      //       )}
+      //     </div>
+
+      //     {currentUser ? (
+      //       <div className="navbar-nav ml-auto">
+      //         <li className="nav-item">
+      //           <Link to={"/profile"} className="nav-link">
+      //             {currentUser.username}
+      //           </Link>
+      //         </li>
+      //         <li className="nav-item">
+      //           <a href="/login" className="nav-link" onClick={this.logOut}>
+      //             LogOut
+      //           </a>
+      //         </li>
+      //       </div>
+      //     ) : (
+      //       <div className="navbar-nav ml-auto">
+      //         <li className="nav-item">
+      //           <Link to={"/login"} className="nav-link">
+      //             Login
+      //           </Link>
+      //         </li>
+
+      //         <li className="nav-item">
+      //           <Link to={"/register"} className="nav-link">
+      //             Sign Up
+      //           </Link>
+      //         </li>
+      //       </div>
+      //     )}
+      //   </nav>
+
+      //   <div className="container mt-3">
+      //     <Routes>
+      //       <Route path="/" element={<Home />} />
+      //       <Route path="/home" element={<Home />} />
+      //       <Route path="/login" element={<Login />} />
+      //       <Route path="/register" element={<Register />} />
+      //       <Route path="/profile" element={<Profile />} />
+      //       <Route path="/user" element={<BoardUser />} />
+      //       <Route path="/mod" element={<BoardModerator />} />
+      //       <Route path="/admin" element={<BoardAdmin />} />
+      //     </Routes>
+      //   </div>
+
+      //   {/* <AuthVerify logOut={this.logOut}/> */}
+      // </div>
+    );
+  }
 }
 
 export default App;
--- src/components/CustomerLogin.js
+++ src/components/CustomerLogin.js
@@ -1,27 +1,27 @@
-import React,{useState} from 'react';
-import AuthenticationService from '../service/AuthenticationService';
-import  {useNavigate} from 'react-router-dom';
-
+import React, { useState } from "react";
+import AuthenticationService from "../service/AuthenticationService";
+import { useNavigate } from "react-router-dom";
+import AuthService from "../service/auth.service";
 
 const CustomerLogin = () => {
-    const history = useNavigate();  // Object to navigate 
-  
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
-  
-    const handleLogin = async () => {
-      if (!email || !password) {
-        setErrorMessage('Please enter both email and password.');
-        return;
-      }
-  
-      const customer = {
-        email,
-        password
-      };
-  
+  const history = useNavigate(); // Object to navigate
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      setErrorMessage("Please enter both email and password.");
+      return;
+    }
+
+    const customer = {
+      email,
+      password,
+    };
+
     /*await is usually used to unwrap promises by passing a Promise as the expression. 
   Using await pauses the execution of its surrounding async function until the promise is 
   settled (that is, fulfilled or rejected).
@@ -31,65 +31,61 @@ const CustomerLogin = () => {
   The Promise object represents the eventual completion (or failure) of an asynchronous 
   operation and its resulting value
      */
-  try {
-      // const loginSuccess = await AuthenticationService.loginCustomer(customer);
-      // console.log('API response:', loginSuccess.data); // Add this line
-      // if (loginSuccess) {
-      //   setSuccessMessage('Login successful. Redirecting...');
-      //   setTimeout(() => {
-      //     // history('/dashboard'); // navigates to product Component
-      //   }, 2000);
-      // } else {
-      //   setErrorMessage('Invalid email or password.');
-      // }
-    const loginSuccess = await fetch("http://localhost:8088/fargoloans/api/login",{
-        method:"POST",
-        headers:{
-          "Content-Type":"applciation/json",  
+    try {
+      AuthService.login(email, password).then(
+        () => {
+          // this.props.router.navigate("/profile");
+          // window.location.reload();
+          setSuccessMessage("Login successful. Redirecting...");
+          history("/dashboard");
         },
-        body:JSON.stringify({
-          customer
-        })
-      })
-
-       if (loginSuccess) {
-        setSuccessMessage('Login successful. Redirecting...');
-        localStorage.setItem("empId", loginSuccess);
-        localStorage.setItem("isAuth", false);
-        
-           history('/dashboard'); // navigates to product Component
-       
-      } else {
-        setErrorMessage('Invalid email or password.');
-      }
-
+        (error) => {
+          setErrorMessage("Invalid email or password.");
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+          console.error("Login error", error);
+          setErrorMessage("An error occurred during login.");
+        }
+      );
     } catch (error) {
-      console.error('Login error', error);
-      setErrorMessage('An error occurred during login.');
+      console.error("Login error", error);
+      setErrorMessage("An error occurred during login.");
     }
-    
-    };
-  
-    return (
-      <div className="container">
-          <br></br>
-        <h2>Customer Login</h2>
-        
-            <div className="form-group w-50">
-                <label>Email</label>
-                <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div className="form-group w-50">
-                <label>Password</label>
-                <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-    
-        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {successMessage && <p className="success-message">{successMessage}</p>}
-        </div> 
-      
-    );
   };
-  
-  export default CustomerLogin;
+
+  return (
+    <div className="container">
+     <h2> Customer Login </h2>
+      <div className="form-group w-50">
+        <label> Email </label>{" "}
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />{" "}
+      </div>{" "}
+      <div className="form-group w-50">
+        <label> Password </label>{" "}
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+      </div>
+      <button className="btn btn-primary" onClick={handleLogin}>
+        {" "}
+        Login{" "}
+      </button>{" "}
+      {errorMessage && <p className="error-message"> {errorMessage} </p>}{" "}
+      {successMessage && <p className="success-message"> {successMessage} </p>}{" "}
+    </div>
+  );
+};
+
+export default CustomerLogin;
